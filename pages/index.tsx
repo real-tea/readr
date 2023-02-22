fd13fdb0a77e49b28135125638f024b5c95bbe21
real-tea <akash.0711a@gmail.com>
2023-02-22 20:13:17 +0530

    rough framework and fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,8 @@ const categories : string[] = [
   "Self" , 
   "Mind" , 
   "Health" , 
-  "World"
+  "World",
+  "Me"
 ]
 
 export default function Home({posts} : any) {
@@ -30,7 +31,42 @@ export default function Home({posts} : any) {
           name="description"
           content="Rich long from essays articles and blogs"
           key="desc"/>
+
+        <meta property="og:title" content="Curated quality long reading" />
+
+        <meta
+          property="og:description"
+          content="Rich long form essays, articles & blogs curated with care."
+        />
+
+        <meta property="og:image" content="" />
       </Head>
+
+      <div className = "mt-40 w-3/4 flex flex-col mx-auto sm:ml-32">
+        <div className="pb-6">
+          {/* <Image/> */}
+          <div className="font-spectral font-medium text-2xl sm:text-3xl text-mygrey mt-4 leading-6 tracking-tight w-4/5 md:w-1/2">
+            Poignant 
+            <span className="font-italics">Essays , Articles & Blogs</span> curated with love and care. 
+          </div>
+        </div>
+      </div>
+
+      <div className="font-spectral pl-[12.5%] pr-[12.5%] sm:ml-32 sm:pl-0 font-medium flex flex-row gap-4 mt-12 overflow-x-scroll no-scrollbar text-mygrey">
+      {categories.map((cat, i) => (
+          <div
+            className={`text-lg md:text-xl cursor-pointer transition-all ease-linear ${
+              category === i ? "opacity-100" : "opacity-50"
+            }`}
+            onClick={() => {
+              setCategory(i);
+            }}
+            key={i}
+          >
+            {cat}
+          </div>
+        ))}
+      </div>
 
     </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        mygrey: "#4D4C4E",
+        mygreylight : "#636263"
+      },
+      fontFamily: {
+        spectral : ['Playfair Display', "serif"]
+      }
+    },
+
+    backgroundImage : {
+      "main-grad" : "linear-gradient(to top , #C7CD2FE , #FECACA , #FFFFFF);",
+      "main-grad-short" : "linear-gradient(to top ,rgba(254 , 202 , 202 , 1)0% , rgba(255, 234, 234, 1) 25%, rgba(255, 255, 255, 1) 100%)",
+
+    }
   },
   plugins: [],
 }
