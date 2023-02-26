024c8d0447b49d40343c1a88a0b1155fdc4f5ff6
real-tea <akash.0711a@gmail.com>
2023-02-26 17:23:52 +0530

    hook for filtering

--- hooks/useCategories.ts
+++ hooks/useCategories.ts
@@ -1,4 +1,5 @@
 export function useCatagories(posts: any, categories: any, category: any) {
+  
   const links = posts.filter((post: any, index: number) => {
     const catArr = post.properties.Category.multi_select.map((item: any) => item.name);
 
